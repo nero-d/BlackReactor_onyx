0235b72f8d723c95ce8b35bbe6ee6a062d9bb893
Nachiket Namjoshi <nachiketnamjoshi@gmail.com>
2017-03-20 23:51:03 +0530

    Remove Fastcharge and all its residues
    [THIS IS A SQUASHED COMMIT]
    Reverting all the fastchg & setcurr related commits
    
    Revert "dwc3: fastchg: Only set Custom Current if it is not 0"
    
    This reverts commit 6cc766d76de4c5f7c407145c59028b66cc38216c.
    
    Revert "fast_charge_current: Added a sysfs node for custom current"
    
    This reverts commit 2752ca82139895546d2d027cda3769501deb8bab.
    
    Revert "usb: Implement USB Fast Charging Driver"
    
    This reverts commit 2f273a97150574e974bb41065ba8e8148fc2fd0e.
    
    Revert "setcurr: check if current value is entered"
    
    This reverts commit 8d200a02b3705051ca71002d823f6299196b1d66.
    
    Remove Setcurr

--- drivers/usb/dwc3/dwc3_otg.c
+++ drivers/usb/dwc3/dwc3_otg.c
@@ -21,10 +21,6 @@
 #include <linux/boot_mode.h>
 #include <linux/kthread.h>
 
-#ifdef CONFIG_FORCE_FAST_CHARGE
-#include <linux/fastchg.h>
-#endif
-
 #include "core.h"
 #include "dwc3_otg.h"
 #include "io.h"
@@ -577,17 +573,7 @@ static int dwc3_otg_set_power(struct usb_phy *phy, unsigned mA)
 
 	if (dotg->charger->max_power == mA)
 		return 0;
- #ifdef CONFIG_FORCE_FAST_CHARGE
- 	if (force_fast_charge > 0 && mA > 0) {
- 		if(force_fast_charge_current <= FAST_CHG_MAX && force_fast_charge_current !=0)
- 			mA = force_fast_charge_current;
- 		else
- 			mA = FAST_CHG_DEFAULT;
- 		pr_info("USB fast charging is ON\n");
- 	} else {
- 		pr_info("USB fast charging is OFF\n");
- 	}
-#endif
+
 	dev_info(phy->dev, "Avail curr from USB = %u\n", mA);
 #ifdef VENDOR_EDIT
       if(get_boot_mode()==MSM_BOOT_MODE__RF){
